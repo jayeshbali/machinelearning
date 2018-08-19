85c29d20288526c94d16ec2551c71ba2463bb166
jayeshbali <41871102+jayeshbali@users.noreply.github.com>
2018-08-19 11:49:09 +0530

    Programming Exercise 3 : Multi-class Classification and Neural Networks Submission

---
+++ machine-learning-ex3/ex3/predictOneVsAll.m
@@ -0,0 +1,51 @@
+function p = predictOneVsAll(all_theta, X)
+%PREDICT Predict the label for a trained one-vs-all classifier. The labels 
+%are in the range 1..K, where K = size(all_theta, 1). 
+%  p = PREDICTONEVSALL(all_theta, X) will return a vector of predictions
+%  for each example in the matrix X. Note that X contains the examples in
+%  rows. all_theta is a matrix where the i-th row is a trained logistic
+%  regression theta vector for the i-th class. You should set p to a vector
+%  of values from 1..K (e.g., p = [1; 3; 1; 2] predicts classes 1, 3, 1, 2
+%  for 4 examples) 
+
+m = size(X, 1);
+num_labels = size(all_theta, 1);
+
+% You need to return the following variables correctly 
+p = zeros(size(X, 1), 1);
+
+% Add ones to the X data matrix
+X = [ones(m, 1) X];
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Complete the following code to make predictions using
+%               your learned logistic regression parameters (one-vs-all).
+%               You should set p to a vector of predictions (from 1 to
+%               num_labels).
+%
+% Hint: This code can be done all vectorized using the max function.
+%       In particular, the max function can also return the index of the 
+%       max element, for more information see 'help max'. If your examples 
+%       are in rows, then, you can use max(A, [], 2) to obtain the max 
+%       for each row.
+%       
+
+pro_sum = X * all_theta'; % Take X*theta' to get vectorised form of the hypothesis
+ % m * num_labels
+pred = max(pro_sum, [], 2); % This will take the max across columns for each row 
+                        % and indicate which of the 10 labels it thinks this 
+                        % example belongs to
+
+% Predict which class the exmaple belongs to e.g. p = [ 0 ... 0 1]                         
+for i = 1: m
+     p(i) = find(pro_sum(i,:) == pred(i));
+end
+
+
+
+
+
+% =========================================================================
+
+
+end
